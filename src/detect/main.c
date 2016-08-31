d685698a06145c3c253c4193ce1fe3b2a68e1e60
Ikey Doherty <ikey@solus-project.com>
2016-08-31 23:56:12 +0100

    Add the initial skeleton so we can start coding
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/detect/main.c
@@ -0,0 +1,17 @@
+/*
+ * This file is part of linux-driver-management.
+ *
+ * Copyright © 2016 Ikey Doherty
+ *
+ * linux-driver-management is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#include <stdio.h>
+
+void placeholder(void)
+{
+        fputs("I am literally just a placeholder.", stdout);
+}
