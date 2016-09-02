29b8d7547e4570e0bf2fab3bde69ae861c99dce5
Ikey Doherty <ikey@solus-project.com>
2016-09-02 02:07:37 +0100

    Check for typos as part of our hygiene processes
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- update_format.sh
+++ update_format.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 clang-format -i $(find . -name '*.[ch]')
+
+# Check we have no typos.
+which misspell 2>/dev/null >/dev/null
+if [[ $? -eq 0 ]]; then
+    misspell -error `find src -name '*.c' -or -name '*.h'`
+fi
