1cdbe198d0f1aa5aed34a82e70019c256d1bf223
Ikey Doherty <ikey@solus-project.com>
2017-03-01 23:02:21 +0000

    Start new skeletal entry program and meson build
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+ * This file is part of linux-driver-management.
+ *
+ * Copyright © 2016-2017 Ikey Doherty
+ *
+ * linux-driver-management is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+        return EXIT_SUCCESS;
+}
