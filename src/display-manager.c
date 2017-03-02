b76baecc0058b4c13d66bdc8716adaddd7cf7531
Ikey Doherty <ikey@solus-project.com>
2017-03-02 23:10:06 +0000

    dm: Add skeletal implementation for GDM, based on LightDM implementation
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/display-manager.c
+++ src/display-manager.c
@@ -12,12 +12,14 @@
 #include "display-manager.h"
 
 extern const LdmDisplayManager lightdm_display_manager;
+extern const LdmDisplayManager gdm_display_manager;
 
 /**
  * Valid DM implementations.
  */
 static const LdmDisplayManager *_managers[] = {
         &lightdm_display_manager, /** LightDM must come before GDM */
+        &gdm_display_manager,     /* Comes last due to others mimicking it */
 };
 
 /**
---
+++ src/dm/gdm.c
@@ -0,0 +1,72 @@
+/*
+ * This file is part of linux-driver-management.
+ *
+ * Copyright © 2016-2017 Ikey Doherty
+ *
+ * linux-driver-management is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#include "../display-manager.h"
+
+#include <unistd.h>
+
+/**
+ * noop
+ */
+static bool gdm_set_xrandr_output(const char *driver, const char *output)
+{
+        return false;
+}
+
+/**
+ * noop
+ */
+static bool gdm_remove_xrandr_output(void)
+{
+        return false;
+}
+
+/**
+ * Determine if LightDM is in use
+ */
+static bool gdm_is_used(void)
+{
+        /* lightdm also sets this but we check lightdm *before* gdm */
+        if (!getenv("GDMSESSION")) {
+                return false;
+        }
+        static const char *gdm_binaries[] = { "/usr/sbin/gdm", "/usr/bin/gdm" };
+        /* Search for gdm and if its accessible, use it */
+        for (size_t i = 0; i < sizeof(gdm_binaries) / sizeof(gdm_binaries[0]); i++) {
+                if (access(gdm_binaries[i], X_OK) == 0) {
+                        return true;
+                }
+        }
+        /* Not gdm */
+        return false;
+}
+
+/**
+ * LightDM implementation of the display manager
+ */
+const LdmDisplayManager gdm_display_manager = {
+        .set_xrandr_output = gdm_set_xrandr_output,
+        .remove_xrandr_output = gdm_remove_xrandr_output,
+        .is_used = gdm_is_used,
+};
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
