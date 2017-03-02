31b4764f0355ed28a6b3266f995bddd3ace0c525
Ikey Doherty <ikey@solus-project.com>
2017-03-02 22:10:48 +0000

    Add skeleton for the DisplayManager type
    This will allow us to easily control multiple types of display manager
    as appropriate. Likely we'll need more vfuncs to handle xrandr situations
    too.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/display-manager.c
@@ -0,0 +1,44 @@
+/*
+ * This file is part of linux-driver-management.
+ *
+ * Copyright © 2016-2017 Ikey Doherty
+ *
+ * linux-driver-management is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#include "display-manager.h"
+
+/**
+ * TODO: Insert actual instances here.
+ */
+static LdmDisplayManager _managers[] = { NULL };
+
+/**
+ * Return the LdmDisplayManager for the current system
+ */
+const LdmDisplayManager const *ldm_display_manager_get_default(void)
+{
+        for (size_t i = 0; i < sizeof(_managers) / sizeof(_managers[0]); i++) {
+                const LdmDisplayManager *manager = &_managers[i];
+                if (manager->is_used()) {
+                        return manager;
+                }
+        }
+        return NULL;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
---
+++ src/display-manager.h
@@ -0,0 +1,55 @@
+/*
+ * This file is part of linux-driver-management.
+ *
+ * Copyright © 2016-2017 Ikey Doherty
+ *
+ * linux-driver-management is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+/**
+ * DisplayManager encapsulates display-manager types
+ * so that they can be configured for use by the drivers
+ */
+typedef struct LdmDisplayManager {
+        /**
+         * Set a configuration item within the DM config
+         */
+        bool (*set_config)(const char *section, const char *key);
+
+        /**
+         * Get a configuration value from the DM config. May return
+         * NULL or an allocated string
+         */
+        char *(*get_config)(const char *section, const char *key, const char *value);
+
+        /**
+         * Determine if the DM is actually used
+         */
+        bool (*is_used)(void);
+} LdmDisplayManager;
+
+/**
+ * Return the LdmDisplayManager for the current system
+ */
+const LdmDisplayManager const *ldm_display_manager_get_default(void);
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
