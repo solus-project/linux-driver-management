a09df1cd511ab4bd42875e12085b94e3bdc09efa
Ikey Doherty <ikey@solus-project.com>
2017-03-05 21:33:45 +0000

    dm/gdm: Finish this with the help of the Arch Wiki
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/dm/gdm.c
+++ src/dm/gdm.c
@@ -9,26 +9,78 @@
  * of the License, or (at your option) any later version.
  */
 
+#include <errno.h>
 #include <unistd.h>
 
 #include "display-manager.h"
 #include "util.h"
 
+static const char *gdm_autostart_paths[] = { "/usr/share/gdm/greeter/autostart/optimus.desktop",
+                                             "/etc/xdg/autostart/optimus.desktop" };
+
+static const char *gdm_autostart_dirs[] = {
+        "/usr/share/gdm/greeter/autostart", "/etc/xdg/autostart",
+};
+
 /**
- * noop
+ * Configure GDM to automatically initialise optimus via xrandr
+ * Inspired/lifted directly from: https://wiki.archlinux.org/index.php/NVIDIA_Optimus#GDM
+ * Many thanks to the Arch Wiki folks
  */
-static bool gdm_set_xrandr_output(__ldm_unused__ const char *driver,
-                                  __ldm_unused__ const char *output)
+static bool gdm_set_xrandr_output(const char *driver, const char *output)
 {
-        return false;
+        /* Create required directories */
+        for (size_t i = 0; i < ARRAY_SIZE(gdm_autostart_dirs); i++) {
+                const char *dir = gdm_autostart_dirs[i];
+                if (mkdir_p(dir, 00755)) {
+                        fprintf(stderr,
+                                "Failed to create autostart dir %s: %s\n",
+                                dir,
+                                strerror(errno));
+                        return false;
+                }
+        }
+        /* Write the same autostart file twice. Once for GDM greeter, once for the
+         * session initialisation itself */
+        for (size_t i = 0; i < ARRAY_SIZE(gdm_autostart_paths); i++) {
+                const char *st = gdm_autostart_paths[i];
+                autofree(FILE) *gdm_conf = NULL;
+
+                gdm_conf = fopen(st, "w+");
+                if (!gdm_conf) {
+                        fprintf(stderr, "Failed to create gdm randr file: %s\n", strerror(errno));
+                        return false;
+                }
+                if (fprintf(gdm_conf,
+                            "[Desktop Entry]\n"
+                            "Type=Application\n"
+                            "Name=Optimus\n"
+                            "Exec=sh -c \"xrandr --setprovideroutputsource %s %s; xrandr --auto\"\n"
+                            "NoDisplay=true\n"
+                            "X-GNOME-Autostart-Phase=DisplayServer\n",
+                            driver,
+                            output) < 0) {
+                        fprintf(stderr, "Failed to write gdm randr file: %s\n", strerror(errno));
+                        return false;
+                }
+        }
+        return true;
 }
 
 /**
- * noop
+ * Remove the autostart files for GDM
  */
 static bool gdm_remove_xrandr_output(void)
 {
-        return false;
+        bool ret = true;
+        for (size_t i = 0; i < ARRAY_SIZE(gdm_autostart_paths); i++) {
+                const char *lfile = gdm_autostart_paths[i];
+
+                if (access(lfile, F_OK) == 0 && unlink(lfile) < 0) {
+                        ret = false;
+                }
+        }
+        return ret;
 }
 
 /**
