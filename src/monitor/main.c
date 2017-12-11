29290d9d420bfac478b6ea2f076ef2b59955e153
Ikey Doherty <ikey@solus-project.com>
2017-12-11 12:48:53 +0000

    monitor: Start fleshing out the GApplication based main loop
    We're going to need notification integration so this calls for a glib
    based service using GApplication and GNotification. We somewhat abuse this
    by turning it into a long running service that will observe hardware change
    events.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/monitor/main.c
+++ src/monitor/main.c
@@ -9,11 +9,68 @@
  * of the License, or (at your option) any later version.
  */
 
-#include <stdlib.h>
+#include <gio/gio.h>
+#include <glib-unix.h>
+#include <stdbool.h>
+
+#define LDM_APP_ID "com.solus-project.linux-driver-management.Monitor"
+
+static bool have_shutdown = false;
+
+/**
+ * We're called on the main context *after* a SIGINT so we don't need
+ * a volatile var. Just ask the application to release so that we can
+ * begin the shutdown process cleanly.
+ */
+static gboolean ldm_shutdown_trigger(GApplication *app)
+{
+        if (have_shutdown) {
+                return FALSE;
+        }
+        have_shutdown = true;
+        g_message("Triggering shutdown");
+        g_application_release(app);
+        return FALSE;
+}
+
+/**
+ * Handle our initial application startup and hook up signals so that
+ * we can safely shut down again using the main context.
+ */
+static void ldm_app_startup(GApplication *app, gpointer v)
+{
+        g_message("App is started");
+
+        /* Safely add shutdown callback. */
+        g_unix_signal_add(SIGINT, (GSourceFunc)ldm_shutdown_trigger, app);
+
+        /* We actually want to run as a background service */
+        g_application_hold(app);
+}
+
+/**
+ * The application was shutdown after g_application_release,
+ * so clean up any existing state.
+ */
+static void ldm_app_shutdown(GApplication *app, gpointer v)
+{
+        g_message("App is deaded.");
+}
 
 int main(int argc, char **argv)
 {
-        return EXIT_FAILURE;
+        GApplication *app = NULL;
+        int ret = 0;
+
+        app = g_application_new(LDM_APP_ID, G_APPLICATION_IS_SERVICE);
+        g_signal_connect(app, "startup", G_CALLBACK(ldm_app_startup), NULL);
+        g_signal_connect(app, "shutdown", G_CALLBACK(ldm_app_shutdown), NULL);
+
+        /* Run the app */
+        ret = g_application_run(app, argc, argv);
+        g_object_unref(app);
+
+        return ret;
 }
 
 /*
