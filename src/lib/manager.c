ea4d9d2432224f8eae62f2fc333a80019bc106a0
Ikey Doherty <ikey@solus-project.com>
2017-12-12 17:22:18 +0000

    Begin skeleton for the new library
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/lib/manager.c
@@ -0,0 +1,68 @@
+/*
+ * This file is part of linux-driver-management.
+ *
+ * Copyright © 2016-2017 Ikey Doherty
+ *
+ * linux-driver-management is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#define _GNU_SOURCE
+
+#include "util.h"
+
+#include "manager.h"
+
+G_DEFINE_TYPE(LdmManager, ldm_manager, G_TYPE_OBJECT)
+
+/**
+ * ldm_manager_dispose:
+ *
+ * Clean up a LdmManager instance
+ */
+static void ldm_manager_dispose(GObject *obj)
+{
+        G_OBJECT_CLASS(ldm_manager_parent_class)->dispose(obj);
+}
+
+/**
+ * ldm_manager_class_init:
+ *
+ * Handle class initialisation
+ */
+static void ldm_manager_class_init(LdmManagerClass *klazz)
+{
+        GObjectClass *obj_class = G_OBJECT_CLASS(klazz);
+
+        /* gobject vtable hookup */
+        obj_class->dispose = ldm_manager_dispose;
+}
+
+/**
+ * ldm_manager_init:
+ *
+ * Handle construction of the LdmManager
+ */
+static void ldm_manager_init(__ldm_unused__ LdmManager *self)
+{
+}
+
+LdmManager *ldm_manager_new()
+{
+        return g_object_new(LDM_TYPE_MANAGER, NULL);
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
---
+++ src/lib/manager.h
@@ -0,0 +1,62 @@
+/*
+ * This file is part of linux-driver-management.
+ *
+ * Copyright © 2016-2017 Ikey Doherty
+ *
+ * linux-driver-management is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+typedef struct _LdmManager LdmManager;
+typedef struct _LdmManagerClass LdmManagerClass;
+
+struct _LdmManagerClass {
+        GObjectClass parent_class;
+
+        gpointer padding[12];
+};
+
+/**
+ * LdmManager is the main library portion of libldm and is used to introspect
+ * devices on the system.
+ */
+struct _LdmManager {
+        GObject parent;
+};
+
+#define LDM_TYPE_MANAGER ldm_manager_get_type()
+#define LDM_MANAGER(o) (G_TYPE_CHECK_INSTANCE_CAST((o), LDM_TYPE_MANAGER, LdmManager))
+#define LDM_IS_MANAGER(o) (G_TYPE_CHECK_INSTANCE_TYPE((o), LDM_TYPE_MANAGER))
+#define LDM_MANAGER_CLASS(o) (G_TYPE_CHECK_CLASS_CAST((o), LDM_TYPE_MANAGER, LdmManagerClass))
+#define LDM_IS_MANAGER_CLASS(o) (G_TYPE_CHECK_CLASS_TYPE((o), LDM_TYPE_MANAGER))
+#define LDM_MANAGER_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS((o), LDM_TYPE_MANAGER, LdmManagerClass))
+
+GType ldm_manager_get_type(void);
+
+/**
+ * Construct a new LdmManager
+ */
+LdmManager *ldm_manager_new(void);
+
+G_END_DECLS
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
