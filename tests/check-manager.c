68029394233b7a37c37706d7760651091c57c4a6
Ikey Doherty <ikey@solus-project.com>
2017-12-13 20:34:31 +0000

    Switch the tests to the new API
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- tests/check-manager.c
+++ tests/check-manager.c
@@ -39,12 +39,12 @@ START_TEST(test_manager_simple)
         manager = ldm_manager_new();
         fail_if(!manager, "Failed to get the LdmManager");
 
-        devices = ldm_manager_get_devices(manager, 0);
+        devices = ldm_manager_get_devices(manager, LDM_DEVICE_TYPE_GPU);
         fail_if(!devices, "Failed to obtain devices");
-        fail_if(g_list_length(devices) != 2, "Invalid device set");
+        fail_if(g_list_length(devices) != 1, "Invalid device set");
 
         /* Grab the NVIDIA device */
-        nvidia_device = g_list_nth_data(devices, 1);
+        nvidia_device = g_list_nth_data(devices, 0);
         fail_if(!ldm_device_has_type(nvidia_device, LDM_DEVICE_TYPE_PCI),
                 "PCI GPU isn't classified as PCI!");
 
@@ -80,12 +80,12 @@ START_TEST(test_manager_optimus)
         manager = ldm_manager_new();
         fail_if(!manager, "Failed to get the LdmManager");
 
-        devices = ldm_manager_get_devices(manager, 0);
+        devices = ldm_manager_get_devices(manager, LDM_DEVICE_TYPE_GPU);
         fail_if(!devices, "Failed to obtain devices");
-        fail_if(g_list_length(devices) != 3, "Invalid device set");
+        fail_if(g_list_length(devices) != 2, "Invalid device set");
 
         /* Check the dGPU data is correct */
-        dgpu = g_list_nth_data(devices, 2);
+        dgpu = g_list_nth_data(devices, 0);
         vendor = ldm_device_get_vendor(dgpu);
         fail_if(!vendor, "No vendor set on dGPU!");
         fail_if(!g_str_equal(vendor, "NVIDIA Corporation"),
@@ -95,7 +95,7 @@ START_TEST(test_manager_optimus)
         vendor = NULL;
 
         /* Check the iGPU data is correct */
-        igpu = g_list_nth_data(devices, 0);
+        igpu = g_list_nth_data(devices, 1);
         vendor = ldm_device_get_vendor(igpu);
         fail_if(!vendor, "No vendor set on iGPU!");
         fail_if(!g_str_equal(vendor, "Intel Corporation"),
