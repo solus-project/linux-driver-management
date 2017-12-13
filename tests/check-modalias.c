d6ec2d71d38ddd031f9df112487082350c933c6d
Ikey Doherty <ikey@solus-project.com>
2017-12-13 18:36:03 +0000

    Fix copy-paste
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- tests/check-modalias.c
+++ tests/check-modalias.c
@@ -1,12 +1,12 @@
 /*
- * This file is part of libuf.
+ * This file is part of linux-driver-management.
  *
- * Copyright © 2017 Ikey Doherty
+ * Copyright © 2016-2017 Ikey Doherty
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * linux-driver-management is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
  */
 
 #define _GNU_SOURCE
