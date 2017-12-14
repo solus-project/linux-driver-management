23e6bf4f4a9d4e43e37f18ab7ccc98149e87fd06
Ikey Doherty <ikey@solus-project.com>
2017-12-14 19:16:10 +0000

    Start work on the new GPUConfig test suite
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ tests/check-gpu-config.c
@@ -0,0 +1,104 @@
+/*
+ * This file is part of linux-driver-management.
+ *
+ * Copyright © 2016-2017 Ikey Doherty
+ *
+ * linux-driver-management is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ */
+
+#define _GNU_SOURCE
+
+#include <check.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <umockdev.h>
+
+#include "ldm-private.h"
+#include "ldm.h"
+#include "util.h"
+
+DEF_AUTOFREE(UMockdevTestbed, g_object_unref)
+
+#define NV_MOCKDEV_FILE TEST_DATA_ROOT "/nvidia1060.umockdev"
+
+static UMockdevTestbed *create_bed_from(const char *mockdevname)
+{
+        UMockdevTestbed *bed = NULL;
+
+        bed = umockdev_testbed_new();
+        fail_if(!umockdev_testbed_add_from_file(bed, mockdevname, NULL),
+                "Failed to create device: %s",
+                mockdevname);
+
+        return bed;
+}
+
+/**
+ * This test is to help us develop composite USB aggregation within the
+ * library so that interfaces are merged into a USB device.
+ */
+START_TEST(test_gpu_config_simple)
+{
+        g_autoptr(LdmManager) manager = NULL;
+        autofree(UMockdevTestbed) *bed = NULL;
+        g_autoptr(GList) devices = NULL;
+        g_autoptr(LdmGPUConfig) gpu = NULL;
+
+        bed = create_bed_from(NV_MOCKDEV_FILE);
+        manager = ldm_manager_new();
+
+        gpu = ldm_gpu_config_new(manager);
+        fail_if(!gpu, "Failed to create GPUConfig");
+}
+END_TEST
+
+/**
+ * Standard helper for running a test suite
+ */
+static int ldm_test_run(Suite *suite)
+{
+        SRunner *runner = NULL;
+        int n_failed = 0;
+
+        runner = srunner_create(suite);
+        srunner_run_all(runner, CK_VERBOSE);
+        n_failed = srunner_ntests_failed(runner);
+        srunner_free(runner);
+
+        return n_failed == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+static Suite *test_create(void)
+{
+        Suite *s = NULL;
+        TCase *tc = NULL;
+
+        s = suite_create(__FILE__);
+        tc = tcase_create(__FILE__);
+        suite_add_tcase(s, tc);
+
+        tcase_add_test(tc, test_gpu_config_simple);
+
+        return s;
+}
+
+int main(__ldm_unused__ int argc, __ldm_unused__ char **argv)
+{
+        return ldm_test_run(test_create());
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
