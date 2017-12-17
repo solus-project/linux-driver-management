adf0786c7e9be6f0104e5aaec99179cfd3f07000
Ikey Doherty <ikey@solus-project.com>
2017-12-17 15:23:03 +0000

    Remove debug - it works
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/lib/manager.c
+++ src/lib/manager.c
@@ -337,7 +337,6 @@ static void ldm_manager_push_device(LdmManager *self, udev_device *device)
 
         /* Don't push the child interface again to the parent, i.e. monitor vs enumerate */
         if (parent && g_hash_table_contains(parent->tree.kids, sysfs_path)) {
-                g_message("naughty!");
                 return;
         }
 
