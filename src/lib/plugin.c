b93c854d7ea2bd4a67a3e3620352c780a11eec03
Ikey Doherty <ikey@solus-project.com>
2017-12-17 19:57:31 +0000

    Whoops.
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/lib/plugin.c
+++ src/lib/plugin.c
@@ -1,9 +1,9 @@
 /*
- * This file is part of linux-plugin-management.
+ * This file is part of linux-driver-management.
  *
  * Copyright © 2016-2017 Ikey Doherty
  *
- * linux-plugin-management is free software; you can redistribute it and/or
+ * linux-driver-management is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1
  * of the License, or (at your option) any later version.
