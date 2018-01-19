f34c702ef47ae45207a561b8b52e6b995a90bfcf
Ikey Doherty <ikey@solus-project.com>
2018-01-19 01:23:24 +0000

    cli: Restore support for basic `status` command
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/cli/status.c
+++ src/cli/status.c
@@ -9,14 +9,110 @@
  * of the License, or (at your option) any later version.
  */
 
-#include "../lib/util.h"
+#include "ldm.h"
+#include "util.h"
+
 #include "cli.h"
 
+#include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * Handle pretty printing of a single device to the display
+ */
+static void print_device(LdmDevice *device)
+{
+        gboolean gpu = FALSE;
+
+        gpu = ldm_device_has_type(device, LDM_DEVICE_TYPE_GPU);
+
+        /* Pretty strings */
+        fprintf(stdout, " \u255E Device Name   : %s\n", ldm_device_get_name(device));
+        fprintf(stdout, " \u255E Manufacturer  : %s\n", ldm_device_get_vendor(device));
+
+        /* Ids */
+        fprintf(stdout, " \u255E Product ID    : %x\n", ldm_device_get_product_id(device));
+        fprintf(stdout,
+                " %s Vendor ID     : %x\n",
+                gpu ? "\u255E" : "\u2558",
+                ldm_device_get_vendor_id(device));
+        if (!gpu) {
+                return;
+        }
+
+        /* GPU Specifics */
+        fprintf(stdout,
+                " \u2558 Boot VGA      : %s\n",
+                ldm_device_has_attribute(device, LDM_DEVICE_ATTRIBUTE_BOOT_VGA) ? "yes" : "no");
+}
+
+/**
+ * Handle pretty printing of the GPU configuration to the display
+ */
+static void print_gpu_config(LdmGPUConfig *config)
+{
+        LdmDevice *primary = NULL, *secondary = NULL;
+
+        primary = ldm_gpu_config_get_primary_device(config);
+        secondary = ldm_gpu_config_get_secondary_device(config);
+
+        if (ldm_gpu_config_has_type(config, LDM_GPU_TYPE_OPTIMUS)) {
+                fputs("NVIDIA Optimus\n", stdout);
+        } else if (ldm_gpu_config_has_type(config, LDM_GPU_TYPE_HYBRID)) {
+                fputs("Hybrid Graphics\n", stdout);
+        } else if (ldm_gpu_config_has_type(config, LDM_GPU_TYPE_CROSSFIRE)) {
+                fputs("AMD Crossfire\n", stdout);
+        } else if (ldm_gpu_config_has_type(config, LDM_GPU_TYPE_SLI)) {
+                fputs("NVIDIA SLI\n", stdout);
+        } else if (ldm_gpu_config_has_type(config, LDM_GPU_TYPE_COMPOSITE)) {
+                fputs("Composite GPU\n", stdout);
+        } else {
+                fputs("Simple GPU configuration\n", stdout);
+        }
+
+        fputs("\n", stdout);
+
+        /* We're only concerned with primary vs secondary devices */
+        fprintf(stdout,
+                " \u2552 Primary GPU%s\n",
+                ldm_gpu_config_has_type(config, LDM_GPU_TYPE_HYBRID) ? " (iGPU)" : "");
+        ;
+        print_device(primary);
+
+        if (!secondary) {
+                return;
+        }
+
+        fprintf(stdout,
+                " \u2552 Primary GPU%s\n",
+                ldm_gpu_config_has_type(config, LDM_GPU_TYPE_HYBRID) ? " (dGPU)" : "");
+        ;
+        print_device(secondary);
+}
 
 int ldm_cli_status(__ldm_unused__ int argc, __ldm_unused__ char **argv)
 {
-        return EXIT_FAILURE;
+        g_autoptr(LdmManager) manager = NULL;
+        g_autoptr(LdmGPUConfig) gpu_config = NULL;
+
+        /* No need for hot plug events */
+        manager = ldm_manager_new(LDM_MANAGER_FLAGS_NO_MONITOR);
+        if (!manager) {
+                fprintf(stderr, "Failed to initialiase LdmManager\n");
+                return EXIT_FAILURE;
+        }
+
+        gpu_config = ldm_gpu_config_new(manager);
+        if (!gpu_config) {
+                fprintf(stderr, "Failed to obtain LdmGPUConfig\n");
+                return EXIT_FAILURE;
+        }
+
+        print_gpu_config(gpu_config);
+
+        /* TODO: Emit any other devices with potential matches */
+
+        return EXIT_SUCCESS;
 }
 
 /*
