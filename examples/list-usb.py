7d406610790100c6deb4ca4a5bccb768a0f3f3cf
Ikey Doherty <ikey@solus-project.com>
2018-01-23 06:22:31 +0000

    Add some helpful documentation to the Python example
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- examples/list-usb.py
+++ examples/list-usb.py
@@ -12,6 +12,18 @@ gi.require_version('Ldm', '0.1')
 from gi.repository import Ldm, GObject
 
 class PretendyPlugin(Ldm.Plugin):
+    """ PretendyPlugin extends the "normal" runtime support for plugins.
+        By default LdmManager will not load and plugins, giving full control
+        to the developer.
+
+        It is usually more desirable to add modalias plugins but it is
+        perfectly reasonable to add plugins that match on vendor + capability.
+
+        Note in Python + GObject to override a virtual method, we prefix the
+        name with "do_". Thus, "->get_provider" is "do_get_provider". It is
+        also OK to return None (NULL) from the method if you don't support
+        the device in your plugin.
+    """
 
     # Not really needed but good practice
     __gtype_name__ = "PretendyPlugin"
@@ -23,6 +35,9 @@ class PretendyPlugin(Ldm.Plugin):
         """ Demonstrate basic matching with custom plugins """
         if not device.has_type(Ldm.DeviceType.AUDIO):
             return None
+
+        # Construct new LdmProvider for this plugin + device,
+        # setting the package/bundle name to "pretendy-package"
         return Ldm.Provider.new(self, device, "pretendy-package")
 
 def main():
